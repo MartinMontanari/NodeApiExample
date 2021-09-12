7e6cad8f2c84c16455e82acd7345ad48fdc3a9b9
Martín Montanari <martinmontanari3@gmail.com>
2021-09-12 20:50:26 -0300

    Winston logger and options was added

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,13 +3,28 @@ import cors from 'express';
 import {log} from 'debug';
 import CommonRoutes from './routes/common.routes';
 import UserRoutes from './routes/user.routes';
+import expressWinston from 'express-winston';
+import winston from "winston";
 
 const app = express();
+
+const loggerOptions: expressWinston.LoggerOptions = {
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize({ all: true })
+    ),
+};
+if(!process.env.DEBUG){
+    loggerOptions.meta = false;
+}
+
+app.use(expressWinston.logger(loggerOptions));
 
 const routes: Array<CommonRoutes> = [];
 app.use(cors());
 app.use(express.json());
-
 
 routes.push(new UserRoutes(app));
 
